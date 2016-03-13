1a32195260770b4c7acfa88309eac493aa82c9fa
Edmund Korley <korley.em@gmail.com>
2016-03-13 15:17:17 -0400

    Scrap keys for Heroku

--- bot.js
+++ bot.js
@@ -1,13 +1,11 @@
 var natural = require('natural');
-
 var Twit = require('twit');
-var keys = require('./keys');
 
 var T = new Twit({
-    consumer_key: process.env.consumer_key || keys.consumer_key,
-    consumer_secret: process.env.consumer_secret || keys.consumer_secret,
-    access_token: process.env.access_token || keys.access_token,
-    access_token_secret: process.env.access_token_secret || keys.access_token_secret,
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret,
 });
 
 var activeTracks = [
