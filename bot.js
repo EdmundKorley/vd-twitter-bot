5a8e34b375d6c6f152df2749111116eee8fdb067
Edmund Korley <korley.em@gmail.com>
2016-07-21 15:22:03 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ var T = new Twit({
     access_token_secret: process.env.access_token_secret,
 });
 
-var activeTracks = [ 'altonsterling', 'peoplesmonday', 'shutitdown', 'freddiegray', 'philandocastile', 'AlvaBraziel' ];
+var activeTracks = [ 'charleskinsey' ];
 
 // Upon call, listen for statuses with hastag
 function streamHastag() {
