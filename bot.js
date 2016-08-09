e39afab027aa67f5783cd92ef6114ed069f70916
Edmund Korley <korley.em@gmail.com>
2016-08-09 08:36:43 -0700

    Add track: #LeekMoss

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ var T = new Twit({
     access_token_secret: process.env.access_token_secret,
 });
 
-var activeTracks = [ 'KorrynGaines' ];
+var activeTracks = [ 'LeekMoss' ];
 
 // Upon call, listen for statuses with hastag
 function streamHastag() {
